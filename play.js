d5f2f194f97e1d3e5a0c83bf0111e610eb994d82
DESKTOP-2P4C3GV\robyn <robynlynnyates@hotmail.com>
2018-10-21 07:36:13 -0400

    Updated changes
    Added character differentiation and updated UI some

---
+++ play.js
@@ -0,0 +1,81 @@
+function charSelect() {
+    window.location = "charSelect.html";
+}
+
+function learnToPlay() {
+    window.location = "LearnToPlay.html";
+}
+
+
+// Executed when DOM is loaded
+$(document).ready(function() {
+    // Executed when select is changed
+    $("select").on('change',function() {
+        var x = this.selectedIndex;
+
+        if (x == "") {
+            $("#submit-button").hide();
+        } else {
+            $("#submit-button").show();
+        }
+    });
+
+    // It must not be visible at first time
+    $("#submit-button").css("display","none");
+}); 
+
+
+var charText;
+var photo;
+
+var PUBLIC_KEY = "a8ef3b47025668c4fdbeb0d36e135028";
+var PRIV_KEY = "5bd44bfb155259974dc0c26fef48c3e04ae1f1f4"
+var ts = new Date().getTime();
+
+
+function getCharacter() {
+
+    var text = document.getElementById("charSelect");
+    charText = text.options[text.selectedIndex].value.replace(" ", "%20");
+    console.log(text.options[text.selectedIndex].value);
+
+    var url = "https://gateway.marvel.com:443/v1/public/characters?name=" + charText;
+
+    var hash = md5(ts + PRIV_KEY + PUBLIC_KEY).toString();
+    $.getJSON(url, {
+        ts : ts,
+        apikey: PUBLIC_KEY,
+        hash: hash
+    }).done(function(data) {
+        console.log(data);
+
+
+        desc = JSON.stringify(data.data.results[0].description, null,2);
+        document.getElementById("desc").innerHTML = desc.substring(1, desc.length - 1);
+
+        photo = JSON.stringify(data.data.results[0].thumbnail.path, null, 2);
+        imgString = photo.substring(1, photo.length - 1) + "/portrait_medium.jpg";
+        document.getElementById("photo").src = photo.substring(1, photo.length - 1) + "/portrait_incredible.jpg";
+
+        imgUrl = photo.substring(1, photo.length - 1) + "/portrait_medium.jpg";
+
+
+        sessionStorage.setItem("char", charText);
+        sessionStorage.setItem("imag", imgString);
+        
+
+    }).fail(function(err) {
+        console.log(err);
+    });
+};
+
+
+
+
+function starter() {
+    document.createElement("image");
+    window.location = "startGame.html";
+
+}
+
+
---
+++ playGame.js
@@ -0,0 +1,182 @@
+var myGamePiece;
+var objectCollected = false;
+var enemyKilled = false;
+character = sessionStorage.getItem("char");
+character = character.replace("%20", " ");
+document.getElementById("charHeader").innerHTML = "Welcome " + character;
+
+function startGame() {
+    console.log(sessionStorage.getItem("imag"));
+    myGameArea.start();
+    console.log(character);
+    if (character == "Thor") {
+        myGamePiece = new component(30, 30, "blue", 30, 120);
+        myObstacle = new component(10, 400, "black", 530, 0); 
+        objectToGet = new component(30, 30, "red", 100, 500);
+        myEnemy = new component(30, 30, "yellow", 600, 10);
+    } else if (character == "Hulk") {
+        myGamePiece = new component(30, 30, "green", 30, 120);
+        myObstacle = new component(10, 400, "black", 530, 0);
+        objectToGet = new component(30, 30, "red", 100, 500);
+        myEnemy = new component(30, 30, "yellow", 600, 10);
+    } else if (character == "Spider-Man") {
+        myGamePiece = new component(30, 30, "red", 30, 120);
+        myObstacle = new component(10, 400, "black", 530, 0);
+        objectToGet = new component(30, 30, "orange", 100, 500);
+        myEnemy = new component(30, 30, "yellow", 600, 10);
+    } else if (character == "Storm") {
+        myGamePiece = new component(30, 30, "gray", 30, 120);
+        myObstacle = new component(10, 400, "black", 530, 0);
+        objectToGet = new component(30, 30, "red", 100, 500);
+        myEnemy = new component(30, 30, "yellow", 600, 10);
+    } else if (character == "Captain America") {
+        myGamePiece = new component(30, 30, "blue", 30, 120);
+        myObstacle = new component(10, 400, "black", 530, 0);
+        objectToGet = new component(30, 30, "orange", 100, 500);
+        myEnemy = new component(30, 30, "yellow", 600, 10);
+    }
+
+
+}
+
+var myGameArea = {
+    canvas : document.createElement("canvas"),
+    start : function() {
+        this.canvas.width = 680;
+        this.canvas.height = 540;
+        this.context = this.canvas.getContext("2d");
+        document.body.replaceChild(this.canvas, document.body.childNodes[4]);
+        this.interval = setInterval(updateGameArea, 20);
+        window.addEventListener('keydown', function (e) {
+            myGameArea.key = e.keyCode;
+        })
+        window.addEventListener('keyup', function (e) {
+            myGameArea.key = false;
+        })
+    }, 
+    clear : function(){
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    }
+}
+
+
+function component(width, height, color, x, y) {
+
+    this.gamearea = myGameArea;
+    this.width = width;
+    this.height = height;
+    this.speedX = 0;
+    this.speedY = 0;    
+    this.x = x;
+    this.y = y;    
+    this.update = function() {
+        ctx = myGameArea.context;
+        ctx.fillStyle = color;
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+    this.newPos = function() {
+        this.x += this.speedX;
+        this.y += this.speedY;        
+    }    
+}
+
+function updateGameArea() {
+    myGameArea.clear();
+
+    if(myGamePiece.x < 680 - 30) {
+        myGamePiece.speedX = -myGamePiece.speedX;
+        myGamePiece.speedY = -myGamePiece.speedY;
+    }
+    if(myGamePiece.y < 540 - 30) {
+        myGamePiece.speedX = -myGamePiece.speedX;
+        myGamePiece.speedY = -myGamePiece.speedY;
+    }
+    if(myGamePiece.x < 0 + 30) {
+        myGamePiece.speedX = -myGamePiece.speedX;
+        myGamePiece.speedY = -myGamePiece.speedY;
+    }
+    if(myGamePiece.y < 0 + 30) {
+        myGamePiece.speedX = -myGamePiece.speedX;
+        myGamePiece.speedY = -myGamePiece.speedY;   
+    }
+
+    if (objectCollected == false) {
+        objectToGet.update();
+    }
+    if (enemyKilled == false) {
+        myEnemy.update();
+    }
+    if (enemyKilled && objectCollected) {
+        winningScreen();
+    }
+    checkForCollision();
+    myGamePiece.newPos();    
+    myGamePiece.update();
+    myObstacle.update();
+}
+
+function moveup() {
+    myGamePiece.speedY -= 10;
+}
+
+function movedown() {
+    myGamePiece.speedY += 10; 
+}
+
+function moveleft() {
+    myGamePiece.speedX -= 10; 
+}
+
+function moveright() {
+    myGamePiece.speedX += 10; 
+}
+
+function attackEnemy() {
+    if (enemyKilled == false) {
+        console.log("test");
+        if (collision(myGamePiece, myEnemy)) {
+            delete enemyKilled;
+            enemyKilled = true;
+        }
+    }
+}
+
+function clearmove() {
+    myGamePiece.speedX = 0;
+    myGamePiece.speedY = 0; 
+}
+
+
+function checkForCollision() {
+    if (objectCollected == false) {
+        if (collision(myGamePiece, objectToGet)) {
+            delete objectToGet;
+            objectCollected = true;
+        } 
+    }
+
+    if (collision(myGamePiece, myObstacle)) {
+        losingScreen();
+    }
+
+}
+
+function collision(object1, object2) {
+    return object1.x < object2.x + object2.width &&
+        object1.x + object1.width > object2.x &&
+        object1.y < object2.y + object2.height &&
+        object1.y + object1.height > object2.y;
+}
+
+function winningScreen() {
+    window.location = "winner.html";
+}
+
+function losingScreen() {
+    window.location = "losing.html";
+}
+
+
+function startAgain() {
+    window.location = "charSelect.html";
+}
